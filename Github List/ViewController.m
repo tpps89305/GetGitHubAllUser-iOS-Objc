5c43000aacd178505596883097b69b72125254e2
Dispy <tpps89305@gmail.com>
2020-10-07 10:28:23 +0800

    Feature made: Download JSON from Github API

--- Github List/ViewController.m
+++ Github List/ViewController.m
@@ -17,6 +17,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // Download JSON from Github API
+    NSURL *url = [NSURL URLWithString:@"https://api.github.com/users"];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionTask *dataTask = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        NSLog(@"%@", json);
+    }];
+    [dataTask resume];
 }
 
 
